334431855bad53fff94254b4acc084e97282b37b
Ari Mardiyanto <arymard7@gmail.com>
2018-09-25 23:39:16 +0700

    Tugas Pertemuan 2

---
+++ LatihanPertemuan2.kt
@@ -0,0 +1,20 @@
+class LatihanPertemuan2{
+fun deret(){
+    for(i in 0..100)
+    {
+        print(i)
+        if(i%10==0)
+        {
+           print("-> Dibagi 10")
+            println()
+        }
+        else if (i%2==0)
+        {
+         print("-> Genap")
+            println()
+        }
+        else
+            println()
+    }
+}
+}
